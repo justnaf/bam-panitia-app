215f5b18cc95c86ef6b6ccb28300c967c8dd5131
justnaf <naufalaf86@gmail.com>
2025-02-01 12:44:56 +0700

    Modify

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,45 +34,50 @@
                         @endisset
                     </div>
                 </div>
-                <div class="ms-3 flex">
-                    <!-- Settings Dropdown -->
-                    <div class="flex">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                    <div>
-                                        {{
-                                    Auth::user()->dataDiri->name ?? Auth::user()->username
-                                }}
-                                    </div>
+                <template x-if="!show">
 
-                                    <div class="ms-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
+                    <div class="ms-3 flex">
+                        <!-- Settings Dropdown -->
+                        <div class="flex">
+                            <x-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                        <div>
+                                            {{
+                                        Auth::user()->dataDiri->name ?? Auth::user()->username
+                                    }}
+                                        </div>
 
-                            <x-slot name="content">
+                                        <div class="ms-1">
+                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                </x-slot>
 
-                                <!-- Authentication -->
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
+                                <x-slot name="content">
 
-                                    <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                        {{ __('Log Out') }}
-                                    </x-dropdown-link>
-                                </form>
-                            </x-slot>
-                        </x-dropdown>
+                                    <!-- Authentication -->
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+
+                                        <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                            {{ __('Log Out') }}
+                                        </x-dropdown-link>
+                                    </form>
+                                </x-slot>
+                            </x-dropdown>
+                        </div>
                     </div>
-                </div>
+                </template>
             </header>
-            <main class="bg-gray-100 flex-1 h-full px-3 py-5">
-                {{ $slot }}
-            </main>
+            <template x-if="!show">
+                <main class="bg-gray-100 flex h-full px-3 py-5">
+                    {{ $slot }}
+                </main>
+            </template>
         </div>
     </div>
     @stack('addedScript')
